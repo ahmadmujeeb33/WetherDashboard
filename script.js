4be717c6b0605c2485f87112b1b676c3d9dd6ff4
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-05-30 17:34:44 -0400

    getting info from API

---
+++ script.js
@@ -0,0 +1,34 @@
+let cityChosen = $('#CityName');
+let searchButton = $('#SerchButton');
+
+const apiKey = "0ee3bf11765f2dbf4429370b2519d0e2";
+
+let queryURL;
+let otherURL;
+
+searchButton.on('click', function(){
+    console.log("this " + cityChosen.val());
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityChosen.val() + "&appid=" + apiKey;
+    
+
+    fetch(queryURL)
+        .then(function(response){
+            return response.json();
+        })
+
+        .then(function(data){
+            console.log(data);
+            otherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data["coord"]["lat"] + "&lon=" + data["coord"]["lon"] + "&appid=" +  apiKey;
+            
+        })
+
+    fetch(otherURL)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        })
+
+})
+
