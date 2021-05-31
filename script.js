9d00b87b0b17c294172d4628e0d0aed070bb88b1
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-05-30 20:30:46 -0400

    in progress of displaying today's wether info

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let cityChosen = $('#CityName');
 let searchButton = $('#SerchButton');
 
+let curentWetherSection = $(".CurrentWether")
+
+let cityTitle = $('<h1>');
+
 const apiKey = "0ee3bf11765f2dbf4429370b2519d0e2";
 
 let queryURL;
 let otherURL;
 
+function DisplayTodayWether(todayWetherInfo){
+    var today = moment();
+    let currentDate = today.format("MMM Do, YYYY");
+
+    console.log("in here");
+    cityTitle.text(cityChosen.val() + " " + currentDate);
+    curentWetherSection.append(cityTitle);
+}
+
 searchButton.on('click', function(){
     console.log("this " + cityChosen.val());
-    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityChosen.val() + "&appid=" + apiKey;
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityChosen.val() + "&units=metric&appid=" + apiKey;
     
 
     fetch(queryURL)
@@ -17,8 +30,8 @@ searchButton.on('click', function(){
         })
 
         .then(function(data){
-            console.log(data);
-            otherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data["coord"]["lat"] + "&lon=" + data["coord"]["lon"] + "&appid=" +  apiKey;
+            DisplayTodayWether(data);
+            otherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data["coord"]["lat"] + "&units=metric&lon=" + data["coord"]["lon"] + "&appid=" +  apiKey;
             fetch(otherURL)
                 .then(function(response){
                     return response.json();
@@ -30,6 +43,8 @@ searchButton.on('click', function(){
         
         
         })
+
+        
 
     
 })
